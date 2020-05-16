d88fccdeb6bd04e1da7586aa83dd93533b81600b
alchern2412 <mynameislyosha2412@yandex.com>
2020-05-16 11:13:38 +0300

    Lab24 WebDav

---
+++ lab24-webdav/server.js
@@ -0,0 +1,19 @@
+// npm i webdav
+// const config = require('config')
+const express = require('express')
+const fileUpload = require('express-fileupload')
+
+const app = express()
+
+app.use(express.json({ extended: false }))
+app.use(fileUpload())
+
+// app.get('/', (req, res) => {
+//     res.send('API Runnig')
+// })
+
+app.use('/api/webdav', require('./routes/api/webdav'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server is running ${PORT}`));
